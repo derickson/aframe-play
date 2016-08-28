8729b9750daa0cfe0b216ea44658c34a59a867d8
derickson <david.erickson@gmail.com>
2016-08-28 12:31:16 -0400

    cleaned up, edge bluring notuseful

--- spectator.js
+++ spectator.js
@@ -1,3 +1,10 @@
+
+/** ################################################
+
+  GREEN SCREEN CODE
+
+#################################################### */
+
 function luminance(r,g,b) {
   return 0.2126*r + 0.7152*g + 0.0722*b;
 }
@@ -23,33 +30,7 @@ function bitOffset(x, y, width) {
   return ((width * y) + x) * 4;
 }
 
-// code modified from http://blog.ivank.net/fastest-gaussian-blur.html
-// source channel, target channel, width, height, radius
-function gaussBlur_1 (scl, tcl, w, h, r) {
-    var rs = Math.ceil(r * 2.57);     // significant radius
-    for(var i=0; i<h; i++) {
-        for(var j=0; j<w; j++) {
-            var val = [0,0,0], wsum = 0;
-            for(var iy = i-rs; iy<i+rs+1; iy++) {
-                for(var ix = j-rs; ix<j+rs+1; ix++) {
-                    var x = Math.min(w-1, Math.max(0, ix));
-                    var y = Math.min(h-1, Math.max(0, iy));
-                    var dsq = (ix-j)*(ix-j)+(iy-i)*(iy-i);
-                    var wght = Math.exp( -dsq / (2*r*r) ) / (Math.PI*2*r*r);
-                    val[0] += scl[((y*w)+x)*4+0] * wght; 
-                    val[1] += scl[((y*w)+x)*4+1] * wght;  
-                    val[2] += scl[((y*w)+x)*4+2] * wght;  
-                    wsum += wght;
-                }
-            }
-            tcl[(i*w+j)*4+0] = Math.round(val[0]/wsum);
-            tcl[(i*w+j)*4+1] = Math.round(val[1]/wsum);  
-            tcl[(i*w+j)*4+2] = Math.round(val[2]/wsum);  
-            tcl[(i*w+j)*4+3] = scl[((y*w)+x)*4+3];               
-        }
-    }
-}
-
+// Gauss Blur code from http://blog.ivank.net/fastest-gaussian-blur.html
 function boxesForGauss(sigma, n)  // standard deviation, number of boxes
 {
     var wIdeal = Math.sqrt((12*sigma*sigma/n)+1);  // Ideal averaging filter width 
@@ -64,6 +45,7 @@ function boxesForGauss(sigma, n)  // standard deviation, number of boxes
     return sizes;
 }
 
+// my code do each color channel separately
 function gaussBlurColor_4(sclData, tclData, w, h, r){
   var sourceR = new Array(w*h);
   var sourceG = new Array(w*h);
@@ -114,7 +96,6 @@ function gaussBlur_4 (scl, tcl, w, h, r) {
 }
 
 
-
 function boxBlur_4 (scl, tcl, w, h, r) {
     for(var i=0; i<scl.length; i++) tcl[i] = scl[i];
     boxBlurH_4(tcl, scl, w, h, r);
@@ -144,16 +125,52 @@ function boxBlurT_4 (scl, tcl, w, h, r) {
 }
 
 
+// is the location at offset cx cy full 255 on green channel?
+function checkGreen(imgData, cx, cy, width, height){
+  if(cx >=0 && cy >=0 && cx < width && cy < height &&
+      imgData.data[bitOffset(cx,cy,width)+1]==255 )
+    return true;
+  else 
+    return false;
+}
+
+// edge detection, check surrounding pixels for green
+function touchingAlphaGreen(imgData, x, y, width, height){
+
+  var retval = false;
+  retval =
+    checkGreen(imgData, x-1, y-1, width, height) ||
+    checkGreen(imgData, x-1, y, width, height) ||
+    checkGreen(imgData, x-1, y+1, width, height) ||
+    checkGreen(imgData, x, y-1, width, height) ||
+    checkGreen(imgData, x, y+1, width, height) ||
+    checkGreen(imgData, x+1, y-1, width, height) ||
+    checkGreen(imgData, x+1, y, width, height) ||
+    checkGreen(imgData, x+1, y+1, width, height);
+
+  return retval;
+}
 
 
 
+/** ################################################
 
-var videoLoaded = false;
+  spectator Component
+
+#################################################### */
 
 
 AFRAME.registerComponent('spectator',{
         'schema': {
-          canvas: {
+          specDiv: {
+            type: 'string',
+            default: ''
+          },
+          camVideo: {
+            type: 'string',
+            default: ''
+          },
+          compDiv: {
             type: 'string',
             default: ''
           },
@@ -161,24 +178,34 @@ AFRAME.registerComponent('spectator',{
           fps: {
             type: 'number',
             default: '15.0'
+          },
+          gaussBlur: {
+            type: 'boolean',
+            default: true
           }
         },
         'init': function() {
 
-          var targetEl = document.querySelector(this.data.canvas)
+          var spectatorEl = document.querySelector(this.data.specDiv)
 
           this.counter = 0;
           this.renderer = new THREE.WebGLRenderer( { antialias: true } );
 
           this.renderer.setPixelRatio( window.devicePixelRatio );
-          this.renderer.setSize( targetEl.offsetWidth, targetEl.offsetHeight );
+          this.renderer.setSize( spectatorEl.offsetWidth, spectatorEl.offsetHeight );
 
           // creates spectator canvas
-          targetEl.appendChild(this.renderer.domElement);
+          spectatorEl.appendChild(this.renderer.domElement);
+
+
+          // init composite canvas
+          var canv = document.createElement('canvas');
+          canv.id = 'videoscreen';
+          document.querySelector(this.data.compDiv).appendChild(canv);
 
 
           // start the webcam
-          var video = document.querySelector("#videoElement");
+          var video = document.querySelector(this.data.camVideo);
           
           navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
           if (navigator.getUserMedia) {       
@@ -199,13 +226,20 @@ AFRAME.registerComponent('spectator',{
         'tick': function(time, timeDelta) {
 
 
+          // -- READABLE PSEUDO CODE --
+          // var loopFPS = 1000.0 / timeDelta;
+          // var hmdIsXFasterThanDesiredFPS = loopFPS / this.data.fps;
+          // var renderEveryNthFrame = Math.round(hmdIsXFasterThanDesiredFPS);
 
-          var loopFPS = 1000.0 / timeDelta;
-          var hmdIsXFasterThanDesiredFPS = loopFPS / this.data.fps;
-          var renderEveryNthFrame = Math.round(hmdIsXFasterThanDesiredFPS);
+          //one line version of the above
+          var renderEveryNthFrame = Math.round(1000.0 / timeDelta / this.data.fps);
+
 
           if(this.counter % renderEveryNthFrame === 0){
             this.render(timeDelta);
+          }
+          if(this.counter === 6000){
+            this.counter = 0
           }
           this.counter += 1;  
         },
@@ -219,13 +253,13 @@ AFRAME.registerComponent('spectator',{
         'renderComposite': function () {
 
           // this is the webcam
-          var foregroundVideo = document.querySelector("#webcam video");
+          var foregroundVideo = document.querySelector(this.data.camVideo);
           
           // this is the spectator camera
-          var backgroundCanvas = document.querySelector("#spectatorDiv canvas");
+          var backgroundCanvas = document.querySelector(this.data.specDiv+" canvas");
 
           // this is the target composite
-          var canvas =  document.querySelector("#composite canvas");
+          var canvas =  document.querySelector("#videoscreen");
 
           var width = foregroundVideo.width;
           var height = foregroundVideo.height;
@@ -248,81 +282,71 @@ AFRAME.registerComponent('spectator',{
             foregroundVideoData = context.getImageData(0, 0, width, height);
             
             var blurredVideoData = context.createImageData(width, height);
+            var alphaData = context.createImageData(width, height);
+
             var imgData = context.createImageData(width, height);
 
-            if(videoLoaded && foregroundVideoData && foregroundVideoData.data){
-
+            if(this.data.gaussBlur)
               gaussBlurColor_4(foregroundVideoData.data, blurredVideoData.data, width, height, 2.0)
-              // context.putImageData(blurredVideoData, 0, 0);
+            
+            for (i = 0; i < imgData.width * imgData.height * 4; i += 4) {
+                var blurryR = blurredVideoData.data[i + 0];
+                var blurryG = blurredVideoData.data[i + 1];
+                var blurryB = blurredVideoData.data[i + 2];
 
-              for (i = 0; i < imgData.width * imgData.height * 4; i += 4) {
-                  var blurryR = blurredVideoData.data[i + 0];
-                  var blurryG = blurredVideoData.data[i + 1];
-                  var blurryB = blurredVideoData.data[i + 2];
+                var r = foregroundVideoData.data[i + 0];
+                var g = foregroundVideoData.data[i + 1];
+                var b = foregroundVideoData.data[i + 2];
+                var a = foregroundVideoData.data[i + 3];
 
-                  var r = foregroundVideoData.data[i + 0];
-                  var g = foregroundVideoData.data[i + 1];
-                  var b = foregroundVideoData.data[i + 2];
-                  var a = foregroundVideoData.data[i + 3];
+                if( (this.data.gaussBlur && isGreenScreen(blurryR,blurryG,blurryB) ) || 
+                    (!this.data.gaussBlur && isGreenScreen(r,g,b) )){  
+                  // alphaData.data[i+0] = 0
+                  // alphaData.data[i+1] = 255
+                  // alphaData.data[i+2] = 0
+                  // alphaData.data[i+3] = 255
 
-                  if( isGreenScreen(blurryR,blurryG,blurryB) ){
-                    a = 0;
+                  imgData.data[i + 0] = imgBackgroundData.data[i + 0];
+                  imgData.data[i + 1] = imgBackgroundData.data[i + 1];
+                  imgData.data[i + 2] = imgBackgroundData.data[i + 2];
+                  imgData.data[i + 3] = imgBackgroundData.data[i + 3];
+                } else { // draw the foreground image
+                  // alphaData.data[i+0] = 0
+                  // alphaData.data[i+1] = 0
+                  // alphaData.data[i+2] = 0
+                  // alphaData.data[i+3] = 255
 
-                    imgData.data[i + 0] = imgBackgroundData.data[i + 0];
-                    imgData.data[i + 1] = imgBackgroundData.data[i + 1];
-                    imgData.data[i + 2] = imgBackgroundData.data[i + 2];
-                    imgData.data[i + 3] = imgBackgroundData.data[i + 3];
-                  }
-                  if(a != 0){ // draw the foreground image
+                  imgData.data[i + 0] = r;
+                  imgData.data[i + 1] = g;
+                  imgData.data[i + 2] = b;
+                  imgData.data[i + 3] = a;
 
-                    imgData.data[i + 0] = r;
-                    imgData.data[i + 1] = g;
-                    imgData.data[i + 2] = b;
-                    imgData.data[i + 3] = a;
-
-                    }
-              }
-
-              // for(y=0; y<height; y++){
-              //   for(x=0; x<width; x++){
-
-              //     bO = bitOffset(x,y,width);
-
-              //     imgData.data[ bO + 0 ] = foregroundVideoData.data[ bO + 0 ];
-              //     imgData.data[ bO + 1 ] = foregroundVideoData.data[ bO + 1 ];
-              //     imgData.data[ bO + 2 ] = foregroundVideoData.data[ bO + 2 ];
-              //     imgData.data[ bO + 3 ] = foregroundVideoData.data[ bO + 3 ];
-
-
-              //   }
-              // }
+                }
             }
 
+            // for( var y = 0; y < height; y++){
+            //   for( var x = 0; x < width; x++){
 
+            //     bO = bitOffset(x,y,width);
 
-            // for (i = 0; i < imgData.width * imgData.height * 4; i += 4) {
-            //     var r = foregroundVideoData.data[i + 0];
-            //     var g = foregroundVideoData.data[i + 1];
-            //     var b = foregroundVideoData.data[i + 2];
-            //     var a = foregroundVideoData.data[i + 3];
+            //     var r = foregroundVideoData.data[bO + 0];
+            //     var g = foregroundVideoData.data[bO + 1];
+            //     var b = foregroundVideoData.data[bO + 2];
+            //     var a = foregroundVideoData.data[bO + 3];
+            //     var gy = gray(r,g,b);
 
-            //     if( isGreenScreen(r,g,b) ){
-            //       a = 0;
+            //     if(touchingAlphaGreen(alphaData,x,y,width,height) && 
+            //       alphaData.data[bO+1]==0) {
 
-            //       imgData.data[i + 0] = imgBackgroundData.data[i + 0];
-            //       imgData.data[i + 1] = imgBackgroundData.data[i + 1];
-            //       imgData.data[i + 2] = imgBackgroundData.data[i + 2];
-            //       imgData.data[i + 3] = imgBackgroundData.data[i + 3];
+            //       // draw greyscale image at edge to reduce color bleeding from green screen
+            //       imgData.data[i + 0] = 255;
+            //       imgData.data[i + 1] = 0;
+            //       imgData.data[i + 2] = 0;
+            //       imgData.data[i + 3] = 255;
             //     }
-            //     if(a != 0){ // draw the foreground image
-            //       imgData.data[i + 0] = r;
-            //       imgData.data[i + 1] = g;
-            //       imgData.data[i + 2] = b;
-            //       imgData.data[i + 3] = a;
-            //     }
+            //   }
             // }
 
-            
             context.putImageData(imgData, 0, 0);
               
           }
